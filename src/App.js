8afd79a35991634e03ec27b599cc970ffed08843
Henrique Brito Elias <henriquebelias@gmail.com>
2021-03-02 18:18:08 -0300

    Exercicio 8

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
         </nav>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
-        <Route path="/users" component={Users} />
+        <Route path="/users" component={() => <Users greetingMessage="Good Morning" />} />
       </BrowserRouter>
     );
   }
